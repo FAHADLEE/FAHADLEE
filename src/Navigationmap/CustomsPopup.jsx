d2912fdf0ef44371a5995d882437051333990b01
Abdul Fahad <fahadlee2000727@gmail.com>
2023-11-16 15:13:46 +0530

    Update for map

--- src/Navigationmap/CustomsPopup.jsx
+++ src/Navigationmap/CustomsPopup.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CustomsPopup.css';
 
 const CustomPopup = ({ formData, onClose, onSubmit, onChange }) => {
+  const [localFormData, setLocalFormData] = useState(formData);
+
+  const handleInputChange = (field, value) => {
+    setLocalFormData((prevFormData) => ({
+      ...prevFormData,
+      [field]: value,
+    }));
+    onChange(field, value);
+  };
+
   return (
     <div className="custom-popup">
       <div className="popup-content">
@@ -10,25 +20,25 @@ const CustomPopup = ({ formData, onClose, onSubmit, onChange }) => {
           <label>
             Day:
             <input
-              type="text"
-              value={formData.day}
-              onChange={(e) => onChange('day', e.target.value)}
+              type="date"
+              value={localFormData.day}
+              onChange={(e) => handleInputChange('day', e.target.value)}
               required
             />
           </label>
           <label>
             Daytime:
             <input
-              type="text"
-              value={formData.daytime}
-              onChange={(e) => onChange('daytime', e.target.value)}
+              type="time"
+              value={localFormData.daytime}
+              onChange={(e) => handleInputChange('daytime', e.target.value)}
             />
           </label>
           <label>
             Type:
             <select
-              value={formData.Type}
-              onChange={(e) => onChange('Type', e.target.value)}
+              value={localFormData.Type}
+              onChange={(e) => handleInputChange('Type', e.target.value)}
             >
               <option value="start">Start</option>
               <option value="end">End</option>
--- src/Navigationmap/NavigationMap.jsx
+++ src/Navigationmap/NavigationMap.jsx
@@ -1,300 +1,148 @@
-import React, { useState, useEffect } from 'react';
-import './NavigationMap.css';
-import axios from 'axios';
-import { GoogleMap, Marker, DirectionsRenderer } from '@react-google-maps/api';
-import CustomsPopup from './CustomsPopup';
-import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
-
-const MapContainer = ({ location, onLoad, onUnmount, onMapClick, clickedLocation, directions, savedDetails, onMarkerClick }) => {
-    const mapStyles = {
-        height: '90vh',
-        width: '100%',
-    };
-    const [map, setMap] = useState(null);
-
-    const handleLoad = (map) => {
-        onLoad(map);
-        setMap(map);
-    };
-    return (
-        <GoogleMap
-            mapContainerStyle={mapStyles}
-            center={location}
-            zoom={15}
-            onLoad={handleLoad}
-            onUnmount={onUnmount}
-            onClick={onMapClick}
-        >
-            {/* Main marker for the current location */}
-            <Marker position={location} />
-
-            {/* Marker for the clicked location */}
-            {clickedLocation && (
-                <Marker
-                    position={clickedLocation}
-                    icon={{ url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png' }}
-                />
-            )}
-            {/* Popup for saved details */}
-            {savedDetails && savedDetails.length > 0 && savedDetails.map((detail, index) => (
-                <div key={index}>
-                    <Marker
-                        position={detail.location}
-                        icon={{ url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png' }}
-                        onClick={() => onMarkerClick(index)} // Pass the index to identify which marker was clicked
-                    />
-                    {index === onMarkerClick && (
-                        <div className='details-popup'>
-                            <input type="date" value={detail.day} />
-                            <input type="time" value={detail.daytime} />
-                            <input type="text" value={detail.Type} />
-                        </div>
-                    )}
-                </div>
-            ))}
-
-            {directions && <DirectionsRenderer directions={directions} options={{ suppressMarkers: true, preserveViewport: true }} />}
-        </GoogleMap>
-    );
-};
-
-
+import React, { useEffect, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDotCircle, faMapMarkerAlt, faMapSigns } from '@fortawesome/free-solid-svg-icons';
+import './Style.css'
 const NavigationMap = () => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [location, setLocation] = useState({ lat: 13.0827, lng: 80.2707 });
-    const [currentDate, setCurrentDate] = useState(new Date());
-    const [popupVisible, setPopupVisible] = useState(false);
-    const [clickedLocation, setClickedLocation] = useState(null);
-    const [startLocation, setStartLocation] = useState(null);
-    const [endLocation, setEndLocation] = useState(null);
-    const [formData, setFormData] = useState({
-        daytime: '',
-        Type: 'start', // Default to 'start'
-        day: '',
-    });
-    const [directions, setDirections] = useState(null);
-    const [savedDetails, setSavedDetails] = useState([]);
-    const [map, setMap] = useState(null);
-
-    const [selectedMarkerIndex, setSelectedMarkerIndex] = useState(null);
-
-    const handleMarkerClick = (index) => {
-        // Handle click on the marker to display details
-        console.log('Marker clicked. Details:', savedDetails[index]);
-        // Set the selected marker index to show details popup
-        setSelectedMarkerIndex(index);
-        setPopupVisible(true);
-    };
+    const inputRef1 = useRef(null);
+    const inputRef2 = useRef(null);
+    const time = useRef(null);
+    const date = useRef(null);
+    const type = useRef(null);
 
     useEffect(() => {
-        // Load saved details from local storage
-        const savedDetails = JSON.parse(localStorage.getItem('savedDetails')) || [];
-        setSavedDetails(savedDetails);
-    }, []);
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(
-                    `https://maps.googleapis.com/maps/api/geocode/json?address=${searchTerm}&key=AIzaSyAKgTjrRsR4GqXNKOp8RNc3WfMaAv8hVSU`
-                );
-                const { lat, lng } = response.data.results[0].geometry.location;
-                setLocation({ lat, lng });
-            } catch (error) {
-                console.error('Error fetching location:', error);
-            }
+        const myLatLng = { lat: 20.5937, lng: 78.9629 }; // India's location
+        const mapOptions = {
+            center: myLatLng,
+            zoom: 7,
+            mapTypeId: window.google.maps.MapTypeId.ROADMAP,
         };
 
-        if (searchTerm) {
-            fetchData();
-        }
+        const map = new window.google.maps.Map(
+            document.getElementById('googleMap'),
+            mapOptions
+        );
 
-        // Update the current date every minute
-        const intervalId = setInterval(() => {
-            setCurrentDate(new Date());
-        }, 60000);
+        const directionsService = new window.google.maps.DirectionsService();
+        const directionsDisplay = new window.google.maps.DirectionsRenderer();
+        directionsDisplay.setMap(map);
 
-        // Set default values for modal form
-        setFormData({
-            daytime: '',
-            Type: 'start', // Default to 'start'
-            day: '',
-        });
-
-        // Clear the interval when the component is unmounted
-        return () => clearInterval(intervalId);
-    }, [searchTerm]);
-
-    const formattedDate = `${currentDate.getFullYear()}/${(currentDate.getMonth() + 1).toString().padStart(2, '0')}/${currentDate.getDate().toString().padStart(2, '0')}`;
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
-        const newDetails = {
-            day: formData.day,
-            daytime: formData.daytime,
-            Type: formData.Type,
-            location: clickedLocation,
-        };
-
-        const updatedDetails = [...savedDetails, newDetails];
-        localStorage.setItem('savedDetails', JSON.stringify(updatedDetails));
-
-        if (startLocation && endLocation) {
-            const directionsService = new window.google.maps.DirectionsService();
-
-            directionsService.route(
-                {
-                    origin: startLocation,
-                    destination: endLocation,
-                    travelMode: 'DRIVING',
-                },
-                (result, status) => {
-                    if (status === 'OK') {
-                        setDirections(result);
-
-                        // Fit the map bounds to the route
-                        const bounds = new window.google.maps.LatLngBounds();
-                        result.routes[0].legs.forEach((leg) => {
-                            leg.steps.forEach((step) => {
-                                step.path.forEach((point) => {
-                                    bounds.extend(point);
-                                });
-                            });
-                        });
-
-                        // Use the 'map' instance obtained from the state
-                        map.fitBounds(bounds);
-                    } else {
-                        console.error(`Directions request failed due to ${status}`);
-                    }
-                }
-            );
-        }
-
-        // Close the popup
-        handlePopupClose();
-    };
-    const handleMapClick = async (e) => {
-        try {
-            const clickedLocation = {
-                lat: e.latLng.lat(),
-                lng: e.latLng.lng(),
+        const calcRoute = () => {
+            const request = {
+                origin: inputRef1.current.value,
+                destination: inputRef2.current.value,
+                travelMode: window.google.maps.TravelMode.DRIVING,
+                unitSystem: window.google.maps.UnitSystem.IMPERIAL,
             };
-            setClickedLocation(clickedLocation);
-            if (formData.Type === 'start') {
-                setStartLocation(clickedLocation);
-            } else if (formData.Type === 'end') {
-                setEndLocation(clickedLocation);
-            }
 
-            const results = await geocodeByAddress(`${clickedLocation.lat},${clickedLocation.lng}`);
-            const address = results[0].formatted_address;
+            directionsService.route(request, (result, status) => {
+                const output = document.querySelector('#output');
+                if (status === window.google.maps.DirectionsStatus.OK) {
+                    output.innerHTML =
+                        `<div class='alert-info'>From: ${inputRef1.current.value}.<br />
+            To: ${inputRef2.current.value}.<br />
+            Driving distance <i class='fas fa-road'></i> : ${result.routes[0].legs[0].distance.text}.<br />
+            Duration <i class='fas fa-hourglass-start'></i> : ${result.routes[0].legs[0].duration.text}.</div>`;
 
-            // Update the search term with the clicked location's address
-            setSearchTerm(address);
-            setPopupVisible(true);
-        } catch (error) {
-            console.error('Error handling map click:', error);
-        }
-    };
+                    directionsDisplay.setDirections(result);
+                } else {
+                    directionsDisplay.setDirections({ routes: [] });
+                    map.setCenter(myLatLng);
 
-    const handlePopupClose = () => {
-        // Close the popup and reset the form data
-        setPopupVisible(false);
-        setClickedLocation(null);
-        setFormData({
-            daytime: '',
-            Type: 'start', // Default to 'start'
-            day: '',
-        });
-    };
+                    output.innerHTML =
+                        `<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance.</div>`;
+                }
+            });
+        };
 
-    // const handleFormSubmit = (e) => {
-    //     e.preventDefault();
-    //     // Add your logic to handle the form submission (e.g., send data to the server)
-    //     console.log('Form data:', formData);
-    //     // Close the popup
-    //     handlePopupClose();
-    // };
+        const options = {
+            types: ['(cities)'],
+        };
 
+        // Initialize Autocomplete for inputRef1
+        const autocomplete1 = new window.google.maps.places.Autocomplete(
+            inputRef1.current,
+            options
+        );
 
+        // Initialize Autocomplete for inputRef2
+        const autocomplete2 = new window.google.maps.places.Autocomplete(
+            inputRef2.current,
+            options
+        );
 
+        const calcRouteButton = document.getElementById('calcRouteButton');
+        calcRouteButton.addEventListener('click', calcRoute);
 
-    const onChange = (field, value) => {
-        // Update the corresponding form data field
-        setFormData({ ...formData, [field]: value });
-    };
-
-    const handleSelect = async (address) => {
-        try {
-            const results = await geocodeByAddress(address);
-            const latLng = await getLatLng(results[0]);
-            setLocation(latLng);
-            setSearchTerm(address);
-        } catch (error) {
-            console.error('Error selecting address:', error);
-        }
-    };
+        // Cleanup function to remove event listener when the component unmounts
+        return () => {
+            calcRouteButton.removeEventListener('click', calcRoute);
+        };
+    }, []);
 
     return (
-        <>
-            <div>
-                <PlacesAutocomplete
-                    className="search-bar-container"
-                    value={searchTerm}
-                    onChange={(newValue) => setSearchTerm(newValue)}
-                    onSelect={handleSelect}
-                >
-                    {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-                        <div className="search-input-field">
-                            <input
-                                {...getInputProps({
-                                    placeholder: 'Enter location',
-                                })}
-                            />
-                            <div>
-                                {loading && <div>Loading...</div>}
-                                {suggestions.map((suggestion) => (
-                                    <div {...getSuggestionItemProps(suggestion)}>{suggestion.description}</div>
-                                ))}
-                            </div>
-                            <p>Current Date: {formattedDate}</p>
-                        </div>
-                    )}
-                </PlacesAutocomplete>
-
-                <MapContainer
-                    location={location}
-                    onLoad={(map) => {
-                        console.log('Map loaded:', map);
-                    }}
-                    onUnmount={(map) => {
-                        console.log('Map unmounted:', map);
-                    }}
-                    onMapClick={handleMapClick}
-                    clickedLocation={clickedLocation}
-                    directions={directions}
-                    savedDetails={savedDetails}
-                    onMarkerClick={handleMarkerClick}
-                />
+        <div>
+            <div style={{ padding: '20px' }}>
+                <form style={{ display: 'flex', flexDirection: 'column', maxWidth: '300px' }}>
+                    <div className='input-field'>
+                        <label htmlFor="from">
+                            <FontAwesomeIcon icon={faDotCircle} /> Start
+                        </label>
+                        <input type="text" id="from" ref={inputRef1} placeholder="Origin" style={{ marginBottom: '10px' }} />
+                        <label htmlFor="from">
+                            <FontAwesomeIcon icon={faDotCircle} /> Time
+                        </label>
+                        <input type="time" id="time" ref={time} placeholder="Time" style={{ marginBottom: '10px' }} />
+                        <label htmlFor="from">
+                            <FontAwesomeIcon icon={faDotCircle} /> Date
+                        </label>
+                        <input type="date" id="date" ref={date} placeholder="Date" style={{ marginBottom: '10px' }} />
+                        <label>
+                            Type:
+                            <select
+                                ref={type}
+                                placeholder="type" style={{ marginBottom: '10px' }}
+                            >
+                                <option value="start">Start</option>
+                                <option value="end">End</option>
+                                <option value="waypoint">Waypoint</option>
+                            </select>
+                        </label>
+                    </div>
+                    <div className='input-field'>
+                        <label htmlFor="to">
+                            <FontAwesomeIcon icon={faMapMarkerAlt} /> End
+                        </label>
+                        <input type="text" id="to" ref={inputRef2} placeholder="Destination" style={{ marginBottom: '10px' }} />
+                        <label htmlFor="from">
+                            <FontAwesomeIcon icon={faDotCircle} /> Time
+                        </label>
+                        <input type="time" id="time" ref={time} placeholder="Time" style={{ marginBottom: '10px' }} />
+                        <label htmlFor="from">
+                            <FontAwesomeIcon icon={faDotCircle} /> Date
+                        </label>
+                        <input type="date" id="date" ref={date} placeholder="Date" style={{ marginBottom: '10px' }} />
+                        <label>
+                            Type:
+                            <select
+                                ref={type}
+                                placeholder="type" style={{ marginBottom: '10px' }}
+                            >
+                                <option value="start">Start</option>
+                                <option value="end">End</option>
+                                <option value="waypoint">Waypoint</option>
+                            </select>
+                        </label>
+                    </div>
+                </form>
+                <button id="calcRouteButton" style={{ padding: '10px', cursor: 'pointer' }}>
+                    <FontAwesomeIcon icon={faMapSigns} /> Route
+                </button>
             </div>
-            <div>
-                {popupVisible && (
-                    <CustomsPopup
-                        formData={formData}
-                        onClose={() => {
-                            // Reset selected marker index and close the popup
-                            setSelectedMarkerIndex(null);
-                            handlePopupClose();
-                        }}
-                        onSubmit={handleFormSubmit}
-                        onChange={onChange}
-                        selectedDetail={selectedMarkerIndex !== null ? savedDetails[selectedMarkerIndex] : null}
-                    />
 
-                )}
-            </div>
-        </>
+            <div id="googleMap" style={{ height: '300px', width: '100%', marginBottom: '20px' }}></div>
+            <div id="output"></div>
+        </div>
     );
 };
 
 export default NavigationMap;
-
